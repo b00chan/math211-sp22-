8f88c99b79bfb24137051a7a8da3b8c400ca3d75
b00chan <103214755+b00chan@users.noreply.github.com>
2023-01-13 15:11:08 -0800

    Add files via upload

---
+++ hw/hw08/tests/q2_2.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q2_2',
+    'points': [0, 9],
+    'suites': [   {   'cases': [   {'code': '>>> 1000 <= true_percentage_intervals <= 10000\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> int(true_percentage_intervals) == 5700\nTrue', 'hidden': True, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ hw/hw08/tests/q2_5.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q2_5',
+    'points': [0, 9],
+    'suites': [   {   'cases': [{'code': '>>> 1 <= cutoff_ten_percent <= 3\nTrue', 'hidden': False, 'locked': False}, {'code': '>>> cutoff_ten_percent == 1\nTrue', 'hidden': True, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
