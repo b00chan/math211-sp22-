97650cd7baf7b17b3b7011edcb8fc2a50d413364
b00chan <103214755+b00chan@users.noreply.github.com>
2023-01-13 14:56:35 -0800

    Add files via upload

---
+++ hw/hw02/tests/q3_2.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q3_2',
+    'points': [0, 4],
+    'suites': [   {   'cases': [   {'code': '>>> # products should be an array!\n>>> len(products) == 4\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> np.allclose(products, np.array([6594, -663168, 66603205994, 39250]))\nTrue', 'hidden': True, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ hw/hw02/tests/q4_1.py
@@ -0,0 +1,10 @@
+test = {   'name': 'q4_1',
+    'points': [0, 0, 2, 2],
+    'suites': [   {   'cases': [   {'code': '>>> # Hint: shortest is a number between 40 and 50.\n>>> 40 <= shortest <= 50\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> # Hint: the average is between the shortest and the longest\n>>> shortest <= average <= longest\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> shortest == 43 and longest == 96\nTrue', 'hidden': True, 'locked': False},
+                                   {'code': '>>> np.isclose(average, 70.8970588235)\nTrue', 'hidden': True, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
