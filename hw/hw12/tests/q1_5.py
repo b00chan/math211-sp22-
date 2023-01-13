20b29a3437ae892e4b54a2d3bbcaaced41006fb7
b00chan <103214755+b00chan@users.noreply.github.com>
2023-01-13 15:13:42 -0800

    Add files via upload

---
+++ hw/hw12/tests/q1_5.py
@@ -0,0 +1,12 @@
+test = {   'name': 'q1_5',
+    'points': [4, 6],
+    'suites': [   {   'cases': [   {   'code': '>>> sorted_coordinates = coordinates.sort("school")\n'
+                                               '>>> classify(sorted_coordinates.row(29), 3, train) == three_classify(sorted_coordinates.row(29))\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {'code': '>>> # You should expect to see really high accuracy\n>>> 0.90 <= accuracy <= 1\nTrue', 'hidden': True, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
