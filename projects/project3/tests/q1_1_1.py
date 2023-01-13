9d13e143149a7df8080a879f6cc22799cfc4636a
b00chan <103214755+b00chan@users.noreply.github.com>
2023-01-13 15:24:55 -0800

    Add files via upload

---
+++ projects/project3/tests/q1_1_1.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q1_1_1',
+    'points': [0, 0, 3],
+    'suites': [   {   'cases': [   {'code': '>>> type(stemmed_message) == str\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': ">>> len(stemmed_message) < len('elements')\nTrue", 'hidden': False, 'locked': False},
+                                   {'code': ">>> stemmed_message\n'element'", 'hidden': True, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
