99c98dcf9f6a50b35246ab05f74770f89eed4a87
b00chan <103214755+b00chan@users.noreply.github.com>
2023-01-14 11:46:18 -0800

    Add files via upload

---
+++ hw/hw03/tests/q2_6.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q2_6',
+    'points': [0, 4],
+    'suites': [   {   'cases': [{'code': '>>> type(assoc) is bool\nTrue', 'hidden': False, 'locked': False}, {'code': '>>> assoc\nTrue', 'hidden': True, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
