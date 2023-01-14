ac52a9339ca0b92f44aec109e9bafc4cb88fe5bc
b00chan <103214755+b00chan@users.noreply.github.com>
2023-01-14 11:53:26 -0800

    Add files via upload

---
+++ hw/hw11/tests/q1_5.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q1_5',
+    'points': [0, 0, 8],
+    'suites': [   {   'cases': [   {'code': '>>> lower_end > 0\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> upper_end < 4\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> lower_end > 0 and upper_end < 4\nTrue', 'hidden': True, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
